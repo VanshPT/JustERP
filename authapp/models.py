8f88624fb54697080d62eb27735126f99678a1a8
Vansh <vanshpthakkar@gmail.com>
2024-05-06 15:16:50 +0530

    CustomUser model Updated

--- authapp/models.py
+++ authapp/models.py
@@ -50,6 +50,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    company=models.ForeignKey(CompanyProfile, on_delete=models.CASCADE, null=True)
     type = models.CharField(max_length=10, choices=[('root', 'Root'), ('normal', 'Normal')], default='normal')
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -58,7 +59,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['company','username', 'first_name', 'last_name']
 
     def __str__(self):
         return self.username
