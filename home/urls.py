272bf5be66d9cbe115c74885e4d6706c23e7faf3
Vansh <vanshpthakkar@gmail.com>
2024-05-23 03:10:22 +0530

    company profile page compeleted, toggle button bug removed

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-  path('<int:company_id>/dashboard/', views.dashboard, name="dashboard"),
-  path('<int:company_id>/company-profile/', views.company_profile, name="Company Profile"),
+  path('dashboard/', views.dashboard, name="dashboard"),
+  path('company-profile/', views.company_profile, name="Company Profile"),
   path('error/', views.error, name="Error")
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from authapp.models import CustomUser,CompanyProfile
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required
-def dashboard(request, company_id):
+def dashboard(request):
     user=request.user
     if user.is_authenticated:
         context={'company_id':user.company.company_id,'company_name':user.company.company_name, 'username':user.username, 'first_name':user.first_name, 'last_name': user.last_name, 'email': user.email}
@@ -18,8 +18,10 @@ def error(request):
    return render(request, 'home/error.html')
 
 @login_required
-def company_profile(request, company_id):
+def company_profile(request):
     user=request.user
     if user.is_authenticated:
-        context={'company_id':user.company.company_id,'company_name':user.company.company_name, 'username':user.username, 'first_name':user.first_name, 'last_name': user.last_name, 'email': user.email}
+        company=get_object_or_404(CompanyProfile,company_id=user.company.company_id)
+
+        context={'company_id':user.company.company_id,'company_name':user.company.company_name, 'username':user.username, 'first_name':user.first_name, 'last_name': user.last_name, 'email': user.email, 'company':company}
     return render(request,'home/companyProfile.html', context)
