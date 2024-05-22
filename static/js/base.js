f7f236548dac99a1f4dfe02b0ee2ae6d270e0e9f
Vansh <vanshpthakkar@gmail.com>
2024-05-23 03:37:06 +0530

    minor css js changes to make popover animation

--- static/js/base.js
+++ static/js/base.js
@@ -45,5 +45,9 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   // popover initialization 
-  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl, {
+      animation: true
+    });
+  });
