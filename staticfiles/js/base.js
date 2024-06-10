0eb63dfa861e1162bbef46ea6b29b973a38212f5
Vansh Thakkar <vanshpthakkar@gmail.com>
2024-06-10 10:37:37 +0530

    static-files copied

---
+++ staticfiles/js/base.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggle-sidebar");
+    const sidebar = document.querySelector("aside");
+    const mainArea = document.querySelector("main");
+    const mainAreaWhite = document.querySelector(".main-area")
+    const header = document.querySelector("header");
+    const custtOuter = document.querySelector(".custt-outer");
+    const logoContainerOff= document.getElementById("toggle-off-logo");
+
+    toggleButton.addEventListener("click", function() {
+      document.body.classList.add("transitioning");
+      if (sidebar.parentNode === document.body) {
+        // If aside element is in the body, remove it
+        document.body.removeChild(sidebar);
+        // Adjust main area width to fill the space
+        mainArea.classList.remove("ml-60");
+        // Adjust header left position
+        header.style.left = "0";
+        // Align the button to the left
+        custtOuter.style.width = "100%";
+        //push main area right
+        mainAreaWhite.style.marginLeft= "15rem";
+        //add justerp symbol when aside toggled off in header
+        logoContainerOff.innerHTML="<h1><span class='main-logo-design'>JustERP</span></h1>";
+
+      } else {
+        // If aside element is not in the body, add it back
+        document.body.appendChild(sidebar);
+        // Restore main area width
+        mainArea.classList.add("ml-60");
+        // Restore header left position
+        header.style.left = "10rem";
+        // Restore button alignment
+        custtOuter.style.width = "auto";
+        //remove justerp symbol when aside toggled off in header
+        logoContainerOff.innerHTML="";
+      }
+      // Set toggle icon margin left to 1rem regardless of the aside state
+      toggleButton.style.marginLeft = "1rem";
+      
+      setTimeout(function() {
+      document.body.classList.remove("transitioning");
+    }, 300);
+
+    });
+  });
+  // popover initialization 
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl, {
+      animation: true
+    });
+  });
